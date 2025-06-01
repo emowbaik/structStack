bebfe73ffffe47cee2ecb47aaeafc17820ac2d35
emowbaik <alamsantoso.12@gmail.com>
2025-06-01 18:22:50 +0800

    add: pointer

--- c030324014_struct.cpp
+++ c030324014_struct.cpp
@@ -8,20 +8,28 @@ using namespace std;
 	string alamat;
 	float ipk;
 };
-int main(){
-	mahasigma alam;
 
-	cout << "NIM = "; 		getline(cin, alam.nim);
-	cout << "Nama = ";		getline(cin, alam.nama);
-	cout << "Alamat = ";	getline(cin, alam.alamat);
-	cout << "IPK = ";		cin >> alam.ipk;
+int inputData(string *nim, string *nama, string *alamat, float *ipk) {
+    cout << "NIM = ";        getline(cin, *nim);
+    cout << "Nama = ";      getline(cin, *nama);
+    cout << "Alamat = ";    getline(cin, *alamat);
+    cout << "IPK = ";       cin >> *ipk;
+    cin.ignore();
+}
 
+void outputData(const mahasigma &alam) {
     system("cls");
-	cout << "\n------------- Data Anda ------------- :\n";
-	cout << "NIM    : " << alam.nim << endl;
-	cout << "Nama   : " << alam.nama << endl;
-	cout << "Alamat : " << alam.alamat << endl;
-	cout << "IPK    : " << alam.ipk << endl;
-	cout << "---------------------------------------\n";
+    cout << "\n------------- Data Anda ------------- :\n";
+    cout << "NIM    : " << alam.nim << endl;
+    cout << "Nama   : " << alam.nama << endl;
+    cout << "Alamat : " << alam.alamat << endl;
+    cout << "IPK    : " << alam.ipk << endl;
+    cout << "---------------------------------------\n";
+}
+
+int main() {
+    mahasigma alam;
+    inputData(&alam.nim, &alam.nama, &alam.alamat, &alam.ipk);
+    outputData(alam);
     return 0;
 }
