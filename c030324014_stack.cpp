3606f7c92928e61e835c58326c07fa72eb524b10
emowbaik <alamsantoso.12@gmail.com>
2025-06-01 18:57:21 +0800

    add: fungsi inisialisasi

--- c030324014_stack.cpp
+++ c030324014_stack.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#define MAXSTACK 10
 
+typedef int tipeItem;
+typedef struct {
+    tipeItem Item[MAXSTACK];
+    int Count;
+} Stack;
+
+void inisialisasi(Stack *S) {
+    S->Count = 0;
+}
 
 
 int main(){
